b3d910b877b57b413c10298fe08fb57ec3530e26
HannaNikola <h.nikolaienko32@gmail.com>
2023-10-24 23:27:22 +0200

    add-component-Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+// import { nanoid } from 'nanoid';
+
+
+const phonebookSchema = Yup.object().shape({
+  name: Yup.string().min(3, 'Too short!').required('This field is required'),
+  number: Yup.number().min(6, 'Too short!').required('This field is required'),
+
+});
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>PhoneBook</h1>
+      <Formik
+        initialValues={{
+          contacts: [],
+          name: '',
+          number: '',
+        }}
+
+        validationSchema={phonebookSchema}
+        onSubmit={values => {
+          console.log(values)
+        }}
+        
+      >
+        
+        <Form>
+          <label htmlFor="name">Name</label>
+            <Field id="name" name="name"/>
+          <ErrorMessage name="name"/>
+        
+
+          <label type="tel" htmlFor="number">Number</label>
+          <Field id="s" name="number"  />
+          <ErrorMessage name="number" />
+          
+          <button type="submit">Add contact</button>
+          </Form>
+      
+      </Formik>
     </div>
   );
 };
